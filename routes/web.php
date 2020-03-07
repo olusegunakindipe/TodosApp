914e87e9d2d7799030e0b1e66b7bac0be19b961f
Fergie <akinfergie@gmail.com>
2020-03-08 05:05:40 +0800

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new', [
+    'uses' => 'PagesController@new'
+]);
+
+Route::get('/todos', [
+    'uses' => 'TodosController@index',
+    'as'=> 'todos'
+]);
+Route::get('/todo/delete/{id}', [
+    'uses' => 'TodosController@delete',
+    'as'=> 'todo.delete'
+]);
+Route::get('/todo/update/{id}', [
+    'uses' => 'TodosController@update',
+    'as'=> 'todo.update'
+]);
+Route::get('/todo/completed/{id}', [
+    'uses' => 'TodosController@completed',
+    'as'=> 'todo.completed'
+]);
+
+Route::post('/create/todo', [
+    'uses' => 'TodosController@store'
+]);
+
+Route::post('/todo/save/{id}', [
+    'uses' => 'TodosController@save',
+    'as'=> 'todos.save'
+]);
